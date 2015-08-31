3abb1164e394f7e7a47fb29dcc6729a1ceedf5a4
sazid <sazidozon@gmail.com>
2015-08-31 19:58:48 +0600

    Basic skeleton code

---
+++ app/src/main/java/com/mohammedsazid/android/launsz/v2/AlphabetsAdapter.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Mohammed Sazid-Al-Rashid
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mohammedsazid.android.launsz.v2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mohammedsazid.android.launsz.R;
+
+public class AlphabetsAdapter extends RecyclerView.Adapter {
+
+    public AlphabetsAdapter() {
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        AlphabetsViewHolder viewHolder = (AlphabetsViewHolder) holder;
+        viewHolder.alphabetTv.setText(String.valueOf(position));
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.alphabets_list_item, parent, false);
+
+        AlphabetsViewHolder viewHolder = new AlphabetsViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 24;
+    }
+
+    static class AlphabetsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView alphabetTv;
+
+        public AlphabetsViewHolder(View itemView) {
+            super(itemView);
+
+            alphabetTv = (TextView) itemView.findViewById(R.id.item_alphabet_textView);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/mohammedsazid/android/launsz/v2/AlphabetsFragment.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Mohammed Sazid-Al-Rashid
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mohammedsazid.android.launsz.v2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mohammedsazid.android.launsz.R;
+
+public class AlphabetsFragment extends Fragment {
+
+    RecyclerView alphabetsRv;
+
+    private void bindViews(View rootView) {
+        alphabetsRv = (RecyclerView) rootView.findViewById(R.id.alphabets_rv);
+    }
+
+    public AlphabetsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alphabets, container, false);
+
+        bindViews(view);
+
+        AlphabetsAdapter adapter = new AlphabetsAdapter();
+        alphabetsRv.setAdapter(adapter);
+        alphabetsRv.setLayoutManager(new GridLayoutManager(
+                getActivity(),
+                4,
+                GridLayoutManager.VERTICAL,
+                false));
+
+        return view;
+    }
+
+}

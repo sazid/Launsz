d49b262c38bd40eb18a050dfa234c679f718b7b7
sazid <sazidozon@gmail.com>
2015-09-01 09:58:42 +0600

    Music control code

--- app/src/main/java/com/mohammedsazid/android/launsz/v2/MainActivity.java
+++ app/src/main/java/com/mohammedsazid/android/launsz/v2/MainActivity.java
@@ -23,11 +23,17 @@
 
 package com.mohammedsazid.android.launsz.v2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.mohammedsazid.android.launsz.R;
 
@@ -37,8 +43,15 @@ public class MainActivity extends FragmentActivity {
     FrameLayout alphabetsFragmentContainer;
     PackageModificationReceiver modificationReceiver;
 
+    ImageView previousTrackIv;
+    ImageView playPauseIv;
+    ImageView nextTrackIv;
+
     private void bindViews() {
         alphabetsFragmentContainer = (FrameLayout) findViewById(R.id.alphabets_fragment_container);
+        previousTrackIv = (ImageView) findViewById(R.id.previousTrackBtn);
+        playPauseIv = (ImageView) findViewById(R.id.playPauseTrackBtn);
+        nextTrackIv = (ImageView) findViewById(R.id.nextTrackBtn);
     }
 
     @Override
@@ -77,6 +90,89 @@ public class MainActivity extends FragmentActivity {
     public void onBackPressed() {
         if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
             getSupportFragmentManager().popBackStack();
+        }
+    }
+
+    public void musicControlCallbacks(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.previousTrackBtn:
+                musicControl("previous");
+                break;
+            case R.id.playPauseTrackBtn:
+                musicControl("playpause");
+                break;
+            case R.id.nextTrackBtn:
+                musicControl("next");
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void musicControl(String mode) {
+        Intent downIntent;
+        Intent upIntent;
+
+        KeyEvent downEvent;
+        KeyEvent upEvent;
+        long eventtime = SystemClock.uptimeMillis();
+
+        final String CMDTOGGLEPAUSE = "togglepause";
+        final String CMDPAUSE = "pause";
+        final String CMDPREVIOUS = "previous";
+        final String CMDNEXT = "next";
+        final String SERVICECMD = "com.android.music.musicservicecommand";
+        final String CMDNAME = "command";
+        final String CMDSTOP = "stop";
+
+        AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        switch (mode) {
+            case "playpause":
+                downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+                downEvent = new KeyEvent(
+                        eventtime,
+                        eventtime,
+                        KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE,
+                        0);
+                downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+                sendOrderedBroadcast(downIntent, null);
+
+                upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+                upEvent = new KeyEvent(
+                        eventtime,
+                        eventtime,
+                        KeyEvent.ACTION_UP,
+                        KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE,
+                        0);
+                upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+                sendOrderedBroadcast(upIntent, null);
+                break;
+            case "stop":
+                if (mAudioManager.isMusicActive()) {
+                    Intent i = new Intent(SERVICECMD);
+                    i.putExtra(CMDNAME, CMDSTOP);
+                    sendBroadcast(i);
+                }
+                break;
+            case "next":
+                if (mAudioManager.isMusicActive()) {
+                    Intent i = new Intent(SERVICECMD);
+                    i.putExtra(CMDNAME, CMDNEXT);
+                    sendBroadcast(i);
+                }
+                break;
+            case "previous":
+                if (mAudioManager.isMusicActive()) {
+                    Intent i = new Intent(SERVICECMD);
+                    i.putExtra(CMDNAME, CMDPREVIOUS);
+                    sendBroadcast(i);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Please use these commands only: 'playpause', 'next', 'stop', 'previous");
         }
     }
 

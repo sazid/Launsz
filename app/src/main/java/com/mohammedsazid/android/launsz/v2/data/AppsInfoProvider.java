44930e3d5a63d271f1b4c445817d310ba312f793
sazid <sazidozon@gmail.com>
2015-09-02 14:47:41 +0600

    Implement query method of provider

--- app/src/main/java/com/mohammedsazid/android/launsz/v2/data/AppsInfoProvider.java
+++ app/src/main/java/com/mohammedsazid/android/launsz/v2/data/AppsInfoProvider.java
@@ -27,17 +27,16 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 public class AppsInfoProvider extends ContentProvider {
-
-    private AppsInfoOpenHelper appsInfoOpenHelper;
 
     // Constants for the content provider
     public static final String CONTENT_AUTHORITY = "com.mohammedsazid.android.launsz.v2.data.AppsInfoProvider";
     public static final String URL = "content://" + CONTENT_AUTHORITY;
     public static final Uri CONTENT_URI = Uri.parse(URL);
-
     // Constants for matching Uris
     private static final int APPSINFO_APPS = 1;
     private static final int APPSINFO_APP = 2;
@@ -48,6 +47,8 @@ public class AppsInfoProvider extends ContentProvider {
         uriMatcher.addURI(CONTENT_AUTHORITY, "/apps", APPSINFO_APPS);
         uriMatcher.addURI(CONTENT_AUTHORITY, "/apps/app/#", APPSINFO_APP);
     }
+
+    private AppsInfoOpenHelper appsInfoOpenHelper;
 
     public AppsInfoProvider() {
     }
@@ -73,17 +74,59 @@ public class AppsInfoProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        appsInfoOpenHelper = new AppsInfoOpenHelper(getContext());
+        try {
+            appsInfoOpenHelper = new AppsInfoOpenHelper(getContext());
+        } catch (Exception e) {
+            return false;
+        }
 
-        return false;
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
         // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = appsInfoOpenHelper.getReadableDatabase();
+        Cursor cursor = null;
+
+        // Set the sorting to be based on launch count (by default)
+        if (sortOrder != null || sortOrder.isEmpty()) {
+            sortOrder = LaunszContract.AppsInfo.COLUMN_LAUNCH_COUNT + " DESC";
+        }
+
+        switch (uriMatcher.match(uri)) {
+            case APPSINFO_APPS:
+                cursor = db.query(
+                        LaunszContract.AppsInfo.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            case APPSINFO_APP:
+                String id = uri.getLastPathSegment();
+                cursor = db.query(
+                        LaunszContract.AppsInfo.TABLE_NAME,
+                        projection,
+                        LaunszContract.AppsInfo._ID + " = ?",
+                        new String[]{id},
+                        null,
+                        null,
+                        sortOrder
+                );
+
+                break;
+        }
+
+        if (cursor == null) {
+            Log.e(AppsInfoProvider.class.getSimpleName(), "Could not load the cursor.");
+        }
+
+        return cursor;
     }
 
     @Override

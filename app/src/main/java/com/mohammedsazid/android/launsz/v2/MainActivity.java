a15d67eb58c61874a3a9900428f1d14f2bee3f74
sazid <sazidozon@gmail.com>
2015-09-18 04:20:46 +0600

    Remove music control and decrease platform dependency to 15

--- app/src/main/java/com/mohammedsazid/android/launsz/v2/MainActivity.java
+++ app/src/main/java/com/mohammedsazid/android/launsz/v2/MainActivity.java
@@ -29,22 +29,18 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.graphics.Color;
-import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.HapticFeedbackConstants;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -62,9 +58,6 @@ public class MainActivity extends FragmentActivity
         implements View.OnLongClickListener {
 
     FrameLayout alphabetsFragmentContainer;
-    ImageView previousTrackIv;
-    ImageView playPauseIv;
-    ImageView nextTrackIv;
     RecyclerView appDockRv;
     TextView appDockHintTv;
     TextView appDockAllAppsTv;
@@ -108,7 +101,7 @@ public class MainActivity extends FragmentActivity
     @Override
     protected void onResume() {
         super.onResume();
-        if (appsService != null && appsService.apps != null) {
+        if (appsService != null && AppsService.apps != null) {
             filterAndShowMostUsedApps();
         }
     }
@@ -172,7 +165,7 @@ public class MainActivity extends FragmentActivity
                         // TODO: Possible optimization point
                         // Loop through all the apps we got from db and match those with the existing ones
                         for (AppInfo appFromDb : appsFromDb) {
-                            for (AppInfo app : appsService.apps) {
+                            for (AppInfo app : AppsService.apps) {
                                 if (appFromDb.name.equals(app.name)) {
                                     app.launchCount = appFromDb.launchCount;
                                     mostUsedApps.add(app);
@@ -199,9 +192,6 @@ public class MainActivity extends FragmentActivity
 
     private void bindViews() {
         alphabetsFragmentContainer = (FrameLayout) findViewById(R.id.alphabets_fragment_container);
-        previousTrackIv = (ImageView) findViewById(R.id.previousTrackBtn);
-        playPauseIv = (ImageView) findViewById(R.id.playPauseTrackBtn);
-        nextTrackIv = (ImageView) findViewById(R.id.nextTrackBtn);
         appDockRv = (RecyclerView) findViewById(R.id.app_dock_rv);
         appDockHintTv = (TextView) findViewById(R.id.app_dock_hint_tv);
         appDockAllAppsTv = (TextView) findViewById(R.id.app_dock_all_apps_tv);
@@ -266,89 +256,6 @@ public class MainActivity extends FragmentActivity
     public void onBackPressed() {
         if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
             getSupportFragmentManager().popBackStack();
-        }
-    }
-
-    public void musicControlCallbacks(View view) {
-        int id = view.getId();
-        switch (id) {
-            case R.id.previousTrackBtn:
-                musicControl("previous");
-                break;
-            case R.id.playPauseTrackBtn:
-                musicControl("playpause");
-                break;
-            case R.id.nextTrackBtn:
-                musicControl("next");
-                break;
-            default:
-                return;
-        }
-    }
-
-    private void musicControl(String mode) {
-        Intent downIntent;
-        Intent upIntent;
-
-        KeyEvent downEvent;
-        KeyEvent upEvent;
-        long eventtime = SystemClock.uptimeMillis();
-
-        final String CMDTOGGLEPAUSE = "togglepause";
-        final String CMDPAUSE = "pause";
-        final String CMDPREVIOUS = "previous";
-        final String CMDNEXT = "next";
-        final String SERVICECMD = "com.android.music.musicservicecommand";
-        final String CMDNAME = "command";
-        final String CMDSTOP = "stop";
-
-        AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        switch (mode) {
-            case "playpause":
-                downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-                downEvent = new KeyEvent(
-                        eventtime,
-                        eventtime,
-                        KeyEvent.ACTION_DOWN,
-                        KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE,
-                        0);
-                downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
-                sendOrderedBroadcast(downIntent, null);
-
-                upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-                upEvent = new KeyEvent(
-                        eventtime,
-                        eventtime,
-                        KeyEvent.ACTION_UP,
-                        KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE,
-                        0);
-                upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
-                sendOrderedBroadcast(upIntent, null);
-                break;
-            case "stop":
-                if (mAudioManager.isMusicActive()) {
-                    Intent i = new Intent(SERVICECMD);
-                    i.putExtra(CMDNAME, CMDSTOP);
-                    sendBroadcast(i);
-                }
-                break;
-            case "next":
-                if (mAudioManager.isMusicActive()) {
-                    Intent i = new Intent(SERVICECMD);
-                    i.putExtra(CMDNAME, CMDNEXT);
-                    sendBroadcast(i);
-                }
-                break;
-            case "previous":
-                if (mAudioManager.isMusicActive()) {
-                    Intent i = new Intent(SERVICECMD);
-                    i.putExtra(CMDNAME, CMDPREVIOUS);
-                    sendBroadcast(i);
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("Please use these commands only: 'playpause', 'next', 'stop', 'previous");
         }
     }
 
